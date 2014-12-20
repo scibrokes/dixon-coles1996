5ac3a533f54929721edcf7d2b8209ff42f447cc9
Eng Lian Hu <englianhu@gmail.com>
2014-12-20 16:54:51 +0800

    update
    moved DC.Rmd and DC_knit2html.R files to knitr and revied the codes

--- knitr/Dixon-Coles_knit2html.R
+++ knitr/Dixon-Coles_knit2html.R
@@ -1,5 +1,5 @@
-mydir <- paste0(getwd(),'/GitHub/englianhu/Dixon-Coles1996') 
-setwd(mydir)
+mydir <- getwd()
+setwd(paste0(getwd(),'/GitHub/englianhu/Dixon-Coles1996/knitr'))
 library(knitr)
 library(markdown)
 
@@ -8,5 +8,6 @@ opts_knit$set(progress=FALSE)
 opts_chunk$set(echo=TRUE, message=FALSE, tidy=TRUE, comment=NA, 
                fig.path="figure/", fig.keep="high", fig.width=10, fig.height=6, 
                fig.align="center") 
-knit2html('knitr/Dixon-Coles1996.Rmd') 
-browseURL('knitr/Dixon-Coles1996.html') 
+knit2html('Dixon-Coles1996.Rmd')
+browseURL('Dixon-Coles1996.html')
+
